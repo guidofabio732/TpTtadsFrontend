23ec40aee9df671da1fcad7719de11121a17d9be
Guido Fabio <guidofabio732@gmail.com>
2021-03-25 21:51:36 -0300

    Succesfuly connected

--- stocker/src/app/app.component.ts
+++ stocker/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TipoMaquinaServiceService } from "./tipo-maquina-service.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stocker';
+  constructor(private api: TipoMaquinaServiceService) {}
+  ngOnInit() {
+    this.api.apiCall().subscribe((data)=>{
+      console.warn("get api data",data)
+      this.title = 'Guido Fa'
+    })
+  }
 }
