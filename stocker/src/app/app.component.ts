11b7ceda9e4adb6eb604a1c9713e06921e8093aa
Guido Fabio <guidofabio732@gmail.com>
2021-03-30 22:53:18 -0300

    Adding

--- stocker/src/app/app.component.ts
+++ stocker/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TipoMaquinaServiceService } from "./tipo-maquina-service.service";
+import { TipoMaquina } from "./models/tipomaquina";
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,10 @@ import { TipoMaquinaServiceService } from "./tipo-maquina-service.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stocker';
-  constructor(private api: TipoMaquinaServiceService) {}
-  ngOnInit() {
-    this.api.apiCall().subscribe((data)=>{
-      console.warn("get api data",data)
-      this.title = 'Guido Fa'
-    })
-  }
+  tiposMaquinaArray: TipoMaquina[] = [
+  {id: 1, descp: "CP"},
+  {id: 2, descp: "F220"},
+  {id: 3, descp: "F300"},
+  {id: 4, descp: "F330"},
+  ];
 }
